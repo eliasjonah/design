fc4607fb8407287843fafb0ab183e3d9da977513
eliasjonah <ilyas_19.04.20@mail.ru>
2023-01-18 14:03:55 +0200

    products section is done

--- src/js/modules/basic-slider.js
+++ src/js/modules/basic-slider.js
@@ -1,15 +1,30 @@
 function basicSlider () {
   // eslint-disable-next-line no-unused-vars
   const swiper = new Swiper('.basic__slider', {
-    centeredSlides: true,
+    // centeredSlides: true,
     loop: true,
     speed: 1500,
-    slidesPerView: '3',
+    slidesPerView: 'auto',
     spaceBetween: 20,
     autoplay: {
       delay: 4000,
       disableOnInteraction: false,
     },
+    breakpoints: {
+      // when window width is >= 720px
+      767: {
+        slidesPerView: 3,
+      },
+      // when window width is >= 576px
+      576: {
+        slidesPerView: 2,
+      },
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 'auto',
+        freeMode: true,
+      },
+    }
   });
 }
 
--- src/js/modules/products-slider.js
+++ src/js/modules/products-slider.js
@@ -5,14 +5,33 @@ function productsSlider () {
     speed: 1500,
     slidesPerView: '4',
     spaceBetween: 20,
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
+    // autoplay: {
+    //   delay: 3000,
+    //   disableOnInteraction: false,
+    // },
     pagination: {
       el: '.products__pagination',
       clickable: true,
     },
+    breakpoints: {
+      // when window width is >= 720px
+      992: {
+        slidesPerView: 4,
+      },
+      // when window width is >= 546px
+      767: {
+        slidesPerView: 3,
+      },
+      // when window width is >= 546px
+      546: {
+        slidesPerView: 2,
+      },
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 'auto',
+        freeMode: true,
+      },
+    }
   });
 }
 
